9177c524cb98d4701f66f15f2f46e4297d7547af
Amanda Allred <amanda.barnett11@yahoo.com>
2025-10-18 14:52:32 -0400

    share header/footer across pages

--- index.js
+++ index.js
@@ -6,3 +6,38 @@ function hamburger() {
         x.style.display = "block"
     }
 }
+
+function addHeader() {
+    const header = document.querySelector(".top-header")
+
+    header.innerHTML =`
+        <figure>
+        <a href="index.html"><img class="logo"src="./images/skillItUpLogo.png"></a>
+            <figcaption></figcaption>
+        </figure>
+        
+        <nav class="navigation">
+            <a href="index.html" class="header light-text nav-btn">Kitchen</a>
+            <a href="recipe.html" class="header light-text nav-btn">Recipes</a>
+            <a href="" class="header light-text nav-btn">Tips & Tricks</a>
+            <!-- <a href="" class="header light-text nav-btn">Share a Recipe</a> -->
+            <!-- <a href="" class="header light-text nav-btn">My Cookbook</a> -->
+
+        </nav>
+        <!-- hamburger menu  -->
+            <a href="javascript:void(0);" class="hamburger" onclick ="hamburger()">
+            <img id="hamburger" src="./images/hamburger.svg.png">
+            </a>
+    `
+}
+
+function addFooter() {
+    const footer = document.querySelector(".footer")
+
+    footer.innerHTML=`
+        <p class="footer-text">Created by Amanda Allred</p>
+`
+}
+
+document.addEventListener("DOMContentLoaded", addHeader)
+document.addEventListener("DOMContentLoaded", addFooter)
