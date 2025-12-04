5fec0bc7466706d6a7fcbe62757bca8105867ba1
Amanda Allred <amanda.barnett11@yahoo.com>
2025-12-03 20:39:34 -0500

    tips populating from json

--- public/js/recipes.js
+++ public/js/recipes.js
@@ -11,15 +11,19 @@ async function getRecipes() {
 
 function createRecipeCard(recipeName) {
     const recipeContainer = document.getElementById("recipe-tiles")
-    const recipeTile = document.createElement("button") // Should these be sections or articles?
+    const recipeTile = document.createElement("button")
     const recipe = document.createElement('h3')
     const recipeLink = document.createElement('a')
 
+    // Add classes
     recipeTile.classList.add("tile", "button")
     recipe.classList.add("header")
+
+    // Populate content
     recipeLink.href = `../recipe.html?id=${recipeName.id}`
     recipeLink.textContent = recipeName.name 
     
+    // Construct tile
     recipe.appendChild(recipeLink) // link added to heading
     recipeTile.appendChild(recipe) // heading added to tile
     recipeContainer.appendChild(recipeTile) // tile added to container
--- public/js/tips.js
+++ public/js/tips.js
@@ -9,4 +9,37 @@ async function getTips() {
     }
 }
 
-getTips()
+function createTipCard(tip) {
+    const tipContainer = document.getElementById("tip-tiles")
+    const tipTile = document.createElement("article")
+    const tipText = document.createElement("p")
+    const tipAuthor = document.createElement("p")
+
+    // Add classes
+    tipTile.classList.add("tile")
+    tipText.classList.add("text")
+    tipAuthor.classList.add("text")
+
+    //Populate content
+    tipText.textContent = tip.tip
+    tipAuthor.textContent = `Shared by: ${tip.name}`
+
+    // Construct tile
+    tipContainer.appendChild(tipTile) // Add tile to parent
+    tipTile.appendChild(tipText) // Tip added to tile
+    tipTile.appendChild(tipAuthor) // Author added to tile
+}
+
+async function renderCards() {
+    try {
+        const tips = await getTips()
+        tips.forEach(tip => 
+            createTipCard(tip)
+        );
+    } catch (error) {
+    console.error(error.message)
+    }
+}
+
+// getTips()
+renderCards()
